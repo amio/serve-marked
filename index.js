02ef0c9e0c63b303b3832e3f6cfa6850088c69e2
Amio <amio.cn@gmail.com>
2018-06-28 17:19:10 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,59 @@
+const fs = require('fs')
+const path = require('path')
+const marked = require('marked')
+
+module.exports = function (mdFile, helmetOptions) {
+  const rawMD = fs.readFileSync(mdFile, 'utf8')
+  const bodyHTML = marked(rawMD)
+  const pageHTML = helmet(bodyHTML, helmetOptions)
+  return function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'})
+    res.end(pageHTML)
+  }
+}
+
+function helmet (bodyHTML, options) {
+  // Custom body wrapper
+  if (typeof options === 'function') {
+    return options(bodyHTML)
+  }
+
+  const { title = '', preset, inlineCSS, googleAnalyticsID } = options
+
+  return `<!DOCTYPE html>
+    <html>
+      <head>
+        <meta charset="utf-8" />
+        <title>${title}</title>
+        ${getPresetStyle(preset)}
+        <style>${inlineCSS}</style>
+        ${generateGAScript(googleAnalyticsID)}
+      </head>
+      <body>
+        ${bodyHTML}
+      </body>
+    </html>
+  `
+}
+
+function getPresetStyle (preset) {
+  const presetNames = ['merri']
+  if (!presetNames.includes(preset)) return ''
+
+  const cssFile = path.join(__dirname, `presets/${preset}.css`)
+  return `<style>${fs.readFileSync(cssFile, 'utf8')}</style>`
+}
+
+function generateGAScript (uaid) {
+  if (typeof uaid !== 'string') return ''
+
+  return `
+    <script async src="https://www.googletagmanager.com/gtag/js?id=${UAID}"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', '${UAID}');
+    </script>
+  `
+}
